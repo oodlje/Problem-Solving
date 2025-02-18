d4e704c1a9ee2ad0e3ed97191e29ad4b01ad95df
jieunlee <143923436+mariewldms@users.noreply.github.com>
2025-02-18 19:50:57 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 604 ms, Memory: 71096 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,34 @@
+import sys
+sys.setrecursionlimit(10**6)
+length=list(map(int,sys.stdin.readline().split()))
+dfsArr=[0]*length[0]
+
+def dfs(start):
+    global count
+    for i in (num[start]):
+        if (visited[i]==0):
+            count+=1
+            dfsArr[i]=count
+            visited[i]=1
+            dfs(i)
+
+num=[[] for _ in range(length[0])]
+visited=[0]*length[0]
+for i in range(length[1]):
+    a,b=map(int,sys.stdin.readline().split())
+    num[a-1].append(b-1)
+    num[b-1].append(a-1)
+
+#역순 구성 
+for i in range(length[0]):
+    num[i].sort()
+
+count=1
+dfsArr[length[2]-1]=count
+visited[length[2]-1]=1
+dfs(length[2]-1)
+
+for i in range(length[0]):
+    print(dfsArr[i])
+
+    
