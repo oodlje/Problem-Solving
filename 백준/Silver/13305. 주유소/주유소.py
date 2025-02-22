f0fdb86fcb6bcbf3722061f6a60862710f305522
jieunlee <143923436+mariewldms@users.noreply.github.com>
2025-02-22 20:24:43 +0900

    [Silver III] Title: 주유소, Time: 100 ms, Memory: 48604 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -4,24 +4,14 @@ edge=list(map(int,sys.stdin.readline().split()))
 node=list(map(int,sys.stdin.readline().split()))
 minNode=node[0]
 sum=[0]*(num)
-cost=0
-# 가장 작은 노드값 구하기
-for i in range(num-1):
-    if (minNode>node[i]):
+cost=node[0]*edge[0]
+# 처음부터 최소 node값을 정해놓고 비용 계산하면 그리디가 아니고
+# 순간에서의 최소 판단이 불가능함 
+# 순간에서의 최소 node값을 이용해 비용 구하기
+for i in range(1, num-1):
+    if (node[i]<minNode):
         minNode=node[i]
-# print(minNode)
-# 각 위치에서의 edge 누적합 구하기
-sum[num-1]=0
-for i in range(num-2,-1,-1):
-    sum[i]=sum[i+1]+edge[i]
-# print(sum)
-# 비용 구하기
-for i in range(num-1):
-    if (node[i]>minNode):
-        cost+=node[i]*edge[i]
-    else:
-        cost+=node[i]*sum[i]
-        break
+    cost+=minNode*edge[i]
 
 print(cost)
 
