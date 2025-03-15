7bacf3aed8b4e560c0a9434ed44e223fb4f64b7b
jieunlee <143923436+mariewldms@users.noreply.github.com>
2025-03-15 23:28:52 +0900

    [Silver III] Title: N과 M (1), Time: 192 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,18 +1,17 @@
 import sys
 n,m=map(int,sys.stdin.readline().split())
-num=[]
-for i in range(n):
-    num.append(i+1)
-
-def numFunc(cur,array):
+array=[]
+cur=0
+def numFunc():
+    global cur
     if (cur==m):
-        # 리스트 자동 공백 문자열 변환
         print(*array)
         return
     for i in range(n):
         if (i+1) not in array:
             array.append(i+1)
-            numFunc(cur+1,array)
+            cur+=1
+            numFunc()
+            cur-=1
             array.pop()
-
-numFunc(0,[])
+numFunc()
