af459877e9ba51e509f854e1a7e5cd66fa2a6734
jieunlee <143923436+mariewldms@users.noreply.github.com>
2025-03-15 23:42:23 +0900

    [Silver III] Title: N과 M (4), Time: 52 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,16 @@
+import sys
+n,m=map(int,sys.stdin.readline().split())
+array=[]
+cur=0
+def numFunc(num):
+    global cur
+    if (cur==m):
+        print(*array)
+        return
+    for i in range(num,n+1):
+        array.append(i)
+        cur+=1
+        numFunc(i)
+        cur-=1
+        array.pop()
+numFunc(1)
