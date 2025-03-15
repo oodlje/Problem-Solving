f6798c594b210aeb67dfd5a45ce3451cca8a9bf5
jieunlee <143923436+mariewldms@users.noreply.github.com>
2025-03-15 23:35:15 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,22 +1,17 @@
 import sys
 n,m=map(int,sys.stdin.readline().split())
-num=[]
-total=set()
-for i in range(n):
-    num.append(i+1)
-
-def numFunc(cur,array):
+array=[]
+cur=0
+def numFunc(num):
+    global cur
     if (cur==m):
-        # 리스트 자동 공백 문자열 변환
-        total.add(frozenset(array))
+        print(*array)
         return
-    for i in range(n):
-        if (i+1) not in array:
-            array.append(i+1)
-            numFunc(cur+1,array)
+    for i in range(num,n+1):
+        if i not in array:
+            array.append(i)
+            cur+=1
+            numFunc(i)
+            cur-=1
             array.pop()
-numFunc(0,[])
-# set을 정렬된 리스트로 변환한 뒤에 전체 요소들을 sort 
-sorted_sets = sorted(total, key=lambda s: sorted(s))
-for s in sorted_sets:
-    print(*sorted(s))
+numFunc(1)
