3de3a22bae6d51aa3cfdac84f55af164c8d7df57
jieunlee <143923436+mariewldms@users.noreply.github.com>
2025-04-02 16:49:14 +0900

    [Bronze III] Title: 곱셈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,13 @@
+import sys
+a=(sys.stdin.readline())
+b=(sys.stdin.readline())
+ans1=int(a)*int(b[2])
+ans2=int(a)*int(b[1])
+ans3=int(a)*int(b[0])
+print(ans1)
+print(ans2)
+print(ans3)
+print(ans1+ans2*10+ans3*100)
+
+
+    
